8dd51db3e34949e653132fb8fe50ed6c977bcab5
matyshinaj <43173617+matyshinaj@users.noreply.github.com>
2021-04-28 12:09:56 +0200

    created card function

--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -1,10 +1,52 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {useState } from "react";
+import { Text, View, StyleSheet, Button, TouchableHighlight } from 'react-native';
 
-export default function Card() {
+export default function Card({ data: { suit, value } }) {
+  console.log("suit = ", suit, " value = ", value);
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(count + 1);
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <TouchableHighlight onPress={onPress}>
+        <View style={styles.button}>
+          <Text>{suit} {value}</Text>
+        </View>
+      </TouchableHighlight>
+      <View style={styles.countContainer}>
+        <Text style={styles.countText}>
+          {count ? count : null}
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 70,
+    height: 70,
+    borderRadius: 10,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    height: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    textAlignVertical: "bottom",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 8
+
+  },
+  countContainer: {
+    alignItems: "center",
+  },
+  countText: {
+    color: "white"
+  }
+});
--- app/src/screens/Game.jsx
+++ app/src/screens/Game.jsx
@@ -2,9 +2,10 @@ import React from "react"
 import { View, Text } from "react-native";
 import { CardData } from "../classes/CardData"
 import { Player } from "../classes/Player"
+import Card from "../components/Card";
 
 const suitCode = ['C', 'B', 'S', 'D'];
-const valueCode = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13'];
+const valueCode = ['01', '02', '03', '04', '05', '06', '07', '11', '12', '13'];
 
 function printCardData(cards) {
   const size = cards.length;
@@ -21,21 +22,32 @@ function getCardDeck() {
   for (let i = 0; i < valueCodeSize; i++) {
     for (let j = 0; j < suitCodeSize; j++) {
       let card = new CardData(suitCode[j], valueCode[i]);
-      console.log(card.suit, card.value);
       cardDeck.push(card);
     }
   }
   return (cardDeck);
 }
 
+function shuffleCardDeck(cards) {
+  cards.sort(() => Math.random() - 0.5);
+  let cardsPlayers = new Array();
+
+  while (cards.length > 0)
+    cardsPlayers.push(cards.splice(0, 10));
+  return (cardsPlayers);
+}
+
 export default function GameScreen() {
   const cardDeck = getCardDeck();
   printCardData(cardDeck);
-  //const players = [new Player('player1', cardsPlayer1), new Player('player2', cardsPlayer2),
-    //new Player('player3', cardsPlayer3), new Player('player4', cardsPlayer4)];
+  let cardsPlayers = shuffleCardDeck(cardDeck);
+  const players = [new Player('player1', cardsPlayers[0]), new Player('player2', cardsPlayers[1]),
+    new Player('player3', cardsPlayers[2]), new Player('player4', cardsPlayers[3])];
+  console.log('PLAYERS:');
+  console.log(players);
   return (
     <View>
-      <Text>GAME SCREEN</Text>
+      {players[0].cards.map(cardData => <Card data={cardData} />)}
     </View>
   );
 }
